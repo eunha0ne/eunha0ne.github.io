6af9af79d1eb7042a34d80ce78894ca719ebbb22
eunha0ne <eunha0ne@gmail.com>
2019-05-21 18:14:06 +0900

    컴포넌트 업데이트

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -22,40 +22,42 @@ const Comments = (props) => {
       script.setAttribute(key, config[key]);
     });
 
-
+    // <기존>
+    // const previousNode = container.current.getElementsByClassName('utterances')[0];
+    // if (previousNode) container.current.removeChild(previousNode);
+    // container.current.appendChild(script);
 
     const params = new URLSearchParams(window.location.search)
 
     // If the 'commenting' URL param is present then scroll to the comments
     if (params.has('commenting')) {
-      const commentsScroll = container.current.getBoundingClientRect().top
-      const scroll = commentsScroll + window.innerHeight
+      const commentsScroll = container.current.getBoundingClientRect().top;
+      const scroll = commentsScroll + window.innerHeight;
       window.scroll(0, scroll)
     }
 
     // Briefly set the 'commenting' URL param while the utterances script is created
     // This ensures the script is initialized with the correct redirect_uri for GitHub OAuth
     params.set('commenting', true)
-    const redirectUrl = `${window.location.pathname}?${params.toString()}`
-    window.history.replaceState(null, '', redirectUrl)
+    const redirectUrl = `${window.location.pathname}?${params.toString()}`;
+    window.history.replaceState(null, '', redirectUrl);
 
     // Append the Utterances script to the container
-    while (container.current.firstChild) container.current.firstChild.remove()
-    container.current.appendChild(script)
-
-    // <기존>
-    // const previousNode = container.current.getElementsByClassName('utterances')[0];
-    // if (previousNode) container.current.removeChild(previousNode);
-    // container.current.appendChild(script);
+    while (container.current.firstChild) {
+      const previousNode = container.current.getElementsByClassName('utterances')[0];
+      container.current.removeChild(previousNode);
+      // container.current.firstChild.remove();
+    }
+    container.current.appendChild(script);
 
     // Once the script has loaded remove the 'commenting' URL param again
     script.onload = () => {
-      params.delete('commenting')
-      const search = params.toString()
+      params.delete('commenting');
+      const search = params.toString();
       const originalUrl = window.location.pathname + (search && `?${search}`)
       window.history.replaceState(null, '', originalUrl)
     }
-  }, [container.current]);
+  },[container.current]);
 
   return <div className="post-comments" ref={container} />
 }
