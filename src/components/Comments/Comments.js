7b135903d4797372a52c1d47ef99501053a0d6a3
eunha0ne <eunha0ne@gmail.com>
2019-10-10 22:00:39 +0900

    Update Comments.js

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,39 +1,39 @@
 import React, { useEffect, useRef } from 'react';
 import { connect } from 'react-redux';
 
-const Comments = (props) => {
+const Comments = props => {
   let container = useRef();
 
   // 기존 헤딩 스타일 중복 제거
   const removeUtterStyleOverlap = () => {
-    const head = document.head;
     const styles = Array.from(document.head.getElementsByTagName('style'));
     const utterStyles = [];
 
-    styles.forEach((node) => {
+    styles.forEach(node => {
       if (node.textContent.indexOf('utterances') !== -1) {
         utterStyles.push(node);
       }
     });
 
-    utterStyles.forEach((node) => {
+    utterStyles.forEach(node => {
       node.parentNode.removeChild(node);
     });
   };
 
   useEffect(() => {
-    if (!container.current) {
+    const currContainer = container.current;
+    if (!currContainer) {
       return;
     }
 
-    const themeType = props.isNightMode ? 'github-dark' : 'github-light';
+    const theme = props.isNightMode ? 'github-dark' : 'github-light';
     const config = {
       src: 'https://utteranc.es/client.js',
       repo: 'eunha0ne/blog-comments',
       'issue-term': 'pathname',
-      theme: themeType,
+      theme: theme,
       async: true,
-      crossorigin: 'anonymous',
+      crossorigin: 'anonymous'
     };
 
     removeUtterStyleOverlap();
@@ -44,40 +44,43 @@ const Comments = (props) => {
     });
 
     // If the 'commenting' URL param is present then scroll to the comments
-    const params = new URLSearchParams(window.location.search)
+    const params = new URLSearchParams(window.location.search);
     if (params.has('commenting')) {
-      const commentsScroll = container.current.getBoundingClientRect().top;
+      const commentsScroll = currContainer.getBoundingClientRect().top;
       const scroll = commentsScroll + window.innerHeight;
-      window.scroll(0, scroll)
+      window.scroll(0, scroll);
     }
 
     // Briefly set the 'commenting' URL param while the utterances script is created
     // This ensures the script is initialized with the correct redirect_uri for GitHub OAuth
-    params.set('commenting', true)
+    params.set('commenting', true);
     const redirectUrl = `${window.location.pathname}?${params.toString()}`;
     window.history.replaceState(null, '', redirectUrl);
 
     // Append the Utterances script to the container
-    while (container.current.firstChild) {
-      const previousNode = container.current.getElementsByClassName('utterances')[0];
-      if (previousNode) container.current.removeChild(previousNode);
-      // container.current.removeChild(previousNode);
-      // container.current.firstChild.remove();
+    while (currContainer.firstChild) {
+      const prevContainer = currContainer.getElementsByClassName(
+        'utterances'
+      )[0];
+      prevContainer && currContainer.removeChild(prevContainer);
     }
-    container.current.appendChild(script);
+    currContainer.appendChild(script);
 
     // Once the script has loaded remove the 'commenting' URL param again
     script.onload = () => {
       params.delete('commenting');
       const search = params.toString();
-      const originalUrl = window.location.pathname + (search && `?${search}`)
-      window.history.replaceState(null, '', originalUrl)
-    }
+      const originalUrl = window.location.pathname + (search && `?${search}`);
+      window.history.replaceState(null, '', originalUrl);
+    };
   });
 
-  return <div className="post-comments" ref={container} />
-}
+  return <div className="post-comments" ref={container} />;
+};
 
-export default connect(state => ({
-  isNightMode: state.app.isNightMode
-}), null)(Comments);
+export default connect(
+  state => ({
+    isNightMode: state.app.isNightMode
+  }),
+  null
+)(Comments);
