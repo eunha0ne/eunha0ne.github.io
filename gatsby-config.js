8547627febb9979e92631152daa608e77422b147
eunha0ne <eunha0ne@gmail.com>
2020-03-17 18:26:57 +0900

    환결 설정 업데이트: emotion (#98)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 const path = require(`path`);
 
 module.exports = {
-  pathPrefix: "/eunha0ne.github.io",
+  pathPrefix: '/eunha0ne.github.io',
   siteMetadata: {
     title: `eunha's dev`,
     description: `은하의 개발 블로그입니다.`,
-    author: `@eunha0ne`,
+    author: `@eunha0ne`
   },
   plugins: [
     {
@@ -21,22 +21,22 @@ module.exports = {
       options: {
         includePaths: [`${__dirname}/src/style`],
         data: `@import 'Utils';`
-      },
+      }
     },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/src/pages/`,
-      },
+        path: `${__dirname}/src/pages/`
+      }
     },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
@@ -63,7 +63,7 @@ module.exports = {
               // you may use this to prevent Prism from re-processing syntax.
               // This is an uncommon use-case though;
               // If you're unsure, it's best to use the default value.
-              classPrefix: "language-",
+              classPrefix: 'language-',
               // This is used to allow setting a language for inline code
               // (i.e. single backticks) by creating a separator.
               // This separator is a string and will do no white-space
@@ -86,13 +86,13 @@ module.exports = {
               showLineNumbers: false,
               // If setting this to true, the parser won't handle and highlight inline
               // code used in markdown i.e. single backtick code like `this`.
-              noInlineHighlight: false,
-            },
+              noInlineHighlight: false
+            }
           },
           {
             resolve: 'gatsby-remark-copy-linked-files',
             options: {
-              destinationDir: 'path/to/dir',
+              destinationDir: 'path/to/dir'
             }
           },
           {
@@ -101,11 +101,11 @@ module.exports = {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
-              maxWidth: 590,
-            },
-          },
-        ],
-      },
+              maxWidth: 590
+            }
+          }
+        ]
+      }
     },
     {
       resolve: `gatsby-plugin-manifest`,
@@ -116,8 +116,8 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
-      },
+        icon: `src/images/favicon.png` // This path is relative to the root of the site.
+      }
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
@@ -125,8 +125,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-145765347-1",
-      },
-    }
+        trackingId: 'UA-145765347-1'
+      }
+    },
+    `gatsby-plugin-emotion`
   ]
-}
+};
